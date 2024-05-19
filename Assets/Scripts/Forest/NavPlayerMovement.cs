c945ed41a8f1ca868fb80b8d5a8d378dff9de009
Glassnose <rollinsasia@yahoo.com>
2024-05-18 23:43:51 -0700

    Initial commit

---
+++ Assets/Scripts/Forest/NavPlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPlayerMovement : MonoBehaviour
+{
+    public float speed = 80.0f;
+    public float rotationSpeed = 30.0f;
+    Rigidbody rgBody = null;
+    float trans = 0;
+    float rotate = 0;
+    private Animator anim;
+
+    public delegate void DropHive(Vector3 pos);
+    public static event DropHive DroppedHive;
+
+    private void Start()
+    {
+        rgBody = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DroppedHive?.Invoke(transform.position + (transform.forward * 10));
+        }
+
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", translation);
+          
+        trans += translation;
+        rotate += rotation;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.y += rotate * rotationSpeed * Time.deltaTime;
+        rgBody.MoveRotation(Quaternion.Euler(rot));
+        rotate = 0;
+
+        Vector3 move = transform.forward * trans;
+        rgBody.velocity = move * speed * Time.deltaTime;
+
+        trans = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Hazard"))
+        {
+            anim.SetTrigger("died");
+            //StartCoroutine(ZoomOut());
+        }
+        else
+        {
+            anim.SetTrigger("twitchLeftEar");
+        }
+    }
+
+    /*IEnumerable ZoomOut()
+    {
+        const int ITERATIONS = 25;
+        for(int z = 0; z < ITERATIONS; z++)
+        {
+            camera.transform.Translate(camera.transform.forward * -1 * 15.0f/ITERATIONS);
+            yield return new WaitForSeconds(1.0f / ITERATIONS);
+        }
+    }*/
+}
